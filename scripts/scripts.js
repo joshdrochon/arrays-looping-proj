e9ec9890b5e31ad65f75f80e141ada8f76d4f154
Josh Rochon <joshdrochon@gmail.com>
2018-01-19 11:31:00 -0800

    add functionality to replace nums % = 0 w string

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,24 +7,26 @@ $(document).ready(function(){
 
     var inputNum = parseInt(prompt("enter a number between 0 & 20"));
 
+    var inputNum = parseInt(prompt("enter a number between 0 & 20"));
+
     var numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; //literal syntax recommended to make arrays
 
-      for(i=0; i <= inputNum; i++){
+          function modifier(){
 
-        function modifier(){
+          	if(numbers[i] % 3 === 0){
+            	console.log(i + ": I'm sorry, Dave!");
+          	}else{
+          		return false;
+          	}
+        	}// end modifier
 
-          if(numbers[i] % 3 === 0){
-            console.log(i + "Sorry!" + "<br"); //will say "Sorry, Dave"
-          }else if(numbers[i].includes(1)){
-            console.log(i + "contains 1" + "<br>"); //will replace all nums containing 1 w/ "boop"
-          }else if(numbers[i].includes(0)){
-            console.log(i + "contains 0" + "<br>"); //will replace all nums container 0 w "beep"
-          }else{
-            return false;
-          }
+          for(i=0; i <= inputNum; i++){
+						modifier();
+					}; //end for loop
 
-        }// end modifier
-      }; //end for loop
 
   }); //end submit handleer
 }); //end doc ready handler
+
+
+var alterNumbers = numbers.map(modifier(numbers))
