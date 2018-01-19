fc49ccc13994e5d2131a85fbe792d530a2ef3b23
Josh Rochon <joshdrochon@gmail.com>
2018-01-19 10:49:31 -0800

    create general blueprint for web app functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,12 +7,24 @@ $(document).ready(function(){
 
     var inputNum = parseInt(prompt("enter a number between 0 & 20"));
 
-    var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    var numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; //literal syntax recommended to make arrays
 
       for(i=0; i <= inputNum; i++){
-        console.log(numbers[i]);
-      };
 
-  });
+        function modifier(){
 
-});
+          if(numbers[i] % 3 === 0){
+            console.log(i + "Sorry!" + "<br"); //will say "Sorry, Dave"
+          }else if(numbers[i].includes(1)){
+            console.log(i + "contains 1" + "<br>"); //will replace all nums containing 1 w/ "boop"
+          }else if(numbers[i].includes(0)){
+            console.log(i + "contains 0" + "<br>"); //will replace all nums container 0 w "beep"
+          }else{
+            return false;
+          }
+
+        }// end modifier
+      }; //end for loop
+
+  }); //end submit handleer
+}); //end doc ready handler
